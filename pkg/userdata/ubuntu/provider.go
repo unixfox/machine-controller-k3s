2fa50a028642b6e4552c426d0ba6ad19ed0828db
Emilien Devos <contact@emiliendevos.be>
2020-11-27 18:22:26 +0200

    wrong backports repo

--- pkg/userdata/ubuntu/provider.go
+++ pkg/userdata/ubuntu/provider.go
@@ -164,7 +164,7 @@ write_files:
     #!/bin/bash
     set -xeuo pipefail
 
-    echo "deb http://http.debian.net/debian stretch-backports main contrib non-free" > /etc/apt/sources.list.d/stretch-backports.list
+    echo "deb http://http.debian.net/debian buster-backports main contrib non-free" > /etc/apt/sources.list.d/buster-backports.list
     DEBIAN_FRONTEND=noninteractive apt-get update
 
     wget -q https://github.com/k0sproject/k0s/releases/download/v0.8.0-rc1/k0s-v0.8.0-rc1-amd64 -O /usr/bin/k0s
